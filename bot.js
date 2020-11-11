6dce22765e0017c618ffd8768f0357c006a11589
Chris Struck <chrisblammo123@gmail.com>
2020-11-11 17:40:53 -0600

    updated some json files, added config example, removed comments from raw event listener work

--- bot.js
+++ bot.js
@@ -3,14 +3,15 @@
  * https://discordapp.com/oauth2/authorize?client_id=507957137465540629&scope=bot&permissions=470019159
  */
 
-const Discord = require('discord.js');        //Discord bot API
-const client = new Discord.Client();          //Discord bot client
+const Discord = require('discord.js');        // Discord bot API
+const client = new Discord.Client({						// Discord bot client
+	partials: ['MESSAGE']												// Allows for the bot to read messages that are not cached
+});
 
 
 const config = require('./config.json');      //Data relating to the discord bot
 const channelInfo = require('./channels.json');      //Data relating to the discord bot
 const roleList = require('./roles.json');      //Data relating to the discord bot
-
 
 
 // Initialization
@@ -76,57 +77,6 @@ client.on('message', (message) => {
 });
 
 
-//client.on('raw', console.log);
-
-
-// client.channels.cache.get(channelInfo.channelID).messages.fetch(channelInfo.messageID);
-
-//(channelInfo.channelID).messages.fetch(channelInfo.messageID, true);
-
-
-
-
-// //Creates events to use for uncached messages, and such.
-// client.on('raw', (packet) => {
-// 	//Removes unnecessary packets
-// 	let validPackets = ['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE']; // might need to expand, so I created this for now
-// console.log(!validPackets.includes(packet.t))
-// 	if (!validPackets.includes(packet.t)) return;
-
-// 	// Gets the channel to check for the message
-// 	let packetChannel = client.channels.get(packet.d.channel_id);
-
-// 	// Checks to see if the message cached in the channel, otherwise it continues
-// 	if (packetChannel.messages.has(packet.d.message_id)) return;
-
-// 	// Fetch the message, since it is not cached
-// 	packetChannel.message.fetch(packet.d.message_id)
-// 		.then(message => {
-// 			// Checks the emoji from the packet and creates a variable identifier for it
-// 			let packetEmoji = packet.d.emoji.id ? `${packet.d.emoji.name}:${packet.d.emoji.id}` : packet.d.emoji.name;
-// console.log(`packetEmoji: ${packetEmoji}`);
-// 			// Checks to see if the message has that reaction
-// 			let packetReaction = message.reactions.get(packetEmoji);
-// console.log(`packetReaction: ${packetReaction}`);
-// 			// Adds the currently reacting user to the reaction's users collection.
-// 			if (packetReaction) packetReaction.users.set(packet.d.user_id, client.users.get(packet.d.user_id));
-
-// 			// Chooses the correct event type and emits it
-// 			if (packet.t == 'MESSAGE_REACTION_ADD') {
-// 				client.emit('messageReactionAdd', packetReaction, client.users.get(packet.d.user_id));
-// 				console.log('add');
-// 			}
-// 			else if (packet.t == 'MESSAGE_REACTION_REMOVE') {
-// 				client.emit('messageReactionRemove', packetReaction, client.users.get(packet.d.user_id));
-// 				console.log('remove');
-// 			}
-// 		});
-// });
-
-
-
-
-
 
 
 
@@ -138,26 +88,6 @@ client.on('messageReactionAdd', (reaction, user) => {
 	console.log(`ADDED:\nreaction: ${reaction} and user: ${user}`);
 });
 
-
-/*
-// Example Packet
-{ t: 'MESSAGE_REACTION_ADD',
-  s: 11,
-  op: 0,
-  d:
-   { user_id: '233679110692405249',
-     message_id: '775542864943513624',
-     member:
-      { user: [Object],
-        roles: [Array],
-        mute: false,
-        joined_at: '2017-09-02T00:30:45.238000+00:00',
-        hoisted_role: '443121113212387349',
-        deaf: false },
-     emoji: { name: '💯', id: null },
-     channel_id: '504285553097965578',
-     guild_id: '353335908038213655' } } 0
-*/
 
 
 
